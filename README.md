1743f565dfd072d2bd1a1583f95a3870f435a5a6
Imene <imene.ouahrani@capgemini.com>
2023-09-04 17:46:38 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# NgPokemonApp
+
+Création d'une application web avec Angular, grâce au tuto de Simon Dieny.
+Lien vers l'application web :  https://hakim-2206.github.io/angular-pokemon-app/
+
+- Découverte d'Angular
+- Générer des composants 
+- Les Templates
+- Les Directives 
+- Les Pipes
+- Les Routes
+- Générer des Services 
+- Formulaires
+- Les Requêtes HTTP
+- RxJS (librairie)
+- Authentification et Sécurité
+  
---
+++ src/app/pokemon/search-pokemon/search-pokemon.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { debounceTime, distinctUntilChanged, Observable, Subject, switchMap } from 'rxjs';
+import { Pokemon } from '../pokemon';
+import { PokemonService } from '../pokemon.service';
+
+@Component({
+  selector: 'app-search-pokemon',
+  templateUrl: './search-pokemon.component.html',
+})
+export class SearchPokemonComponent implements OnInit {
+  // {..."a".."ab"..."abz"..."ab"..."abc"....}
+  searchTerms = new Subject<string>();
+  // {...pokemonList(a)...pokemonList(ab)....}
+  pokemons$: Observable<Pokemon[]>;
+
+  constructor(private router: Router, private pokemonService: PokemonService) { }
+
+  ngOnInit(): void {
+    this.pokemons$ = this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term) => this.pokemonService.searchPokemonList(term))
+    );
+  }
+
+  search(term: string) {
+    this.searchTerms.next(term);
+  }
+
+  goToDetail(pokemon: Pokemon) {
+    const link = ['/pokemon', pokemon.id];
+    this.router.navigate(link);
+  }
+
+}
